ad1b79d274b36ba6da4ab0acaa1a5c80b1ec2d7d
Artem Vsyliev <artemass3@gmail.com>
2025-11-16 11:22:56 +0100

    fix login page redirect

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -42,13 +42,13 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/error')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/'
     return NextResponse.redirect(url)
   }
 
